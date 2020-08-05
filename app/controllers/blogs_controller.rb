509f114905cda91f2e4671d0fccebb13bb39135d
yuki takagi <yuki99takagi@gmail.com>
2020-08-05 16:15:11 +0900

    Add : ブログ編集機能を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+
   def index
     @blogs = Blog.all
   end
@@ -17,11 +19,25 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
