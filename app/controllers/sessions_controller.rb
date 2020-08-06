b61ffed9bba981ad3d7b56d64cf2e454a0167dc2
yuki takagi <yuki99takagi@gmail.com>
2020-08-06 10:50:18 +0900

    Add : ログイン用のヘルパーメソッドを定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'ログインに失敗しました！もう一度入力してください'
+      render :new
+    end
+  end
 end
