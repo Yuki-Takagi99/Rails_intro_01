431a01c1b5e6b2159831cfb308ef3cfc664d51a5
yuki takagi <yuki99takagi@gmail.com>
2020-08-06 13:26:28 +0900

    Add : Favoriteモデル作成、アソシエーション定義

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  # ブログをお気に入りしたユーザーの情報を取得
+  has_many :favorite_users, through: :favorites, source: :user
 end
